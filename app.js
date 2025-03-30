c52802365f7c88a99bcc6e9506b122a94ffd5761
GulievKamran <kamranquliyev7778@gmail.com>
2025-03-30 13:19:00 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// const username = "Kamran";
+// const surname = "Guliev";
+// const education = "Harvard";
+// const pet = "lucky seahorse";
+// const age= 5;
+// const msg=("i am" + " " + username +" "+ surname +  "." +"i have graduated from" +" "+ education +" and my pet is" + " " + pet +age );
+// console.log(msg);
+
+
+const name= prompt("Welcome! Whats your name?");
+const country=prompt("Where are you from?")
+const emotion=prompt("How r u feelin rn?")
+const agree= prompt("Do you agree to our policy guidelines?");
+const msj=("My name is " + name +"."+" " +"I am from "+ country+"."+ " I am feeling " +emotion+"."+ " Yes,i agree ")
+console.log(msj);
+
+const isAgree=confirm("Are you ready?");
+console.log(isAgree);
+ 
+
+
+
